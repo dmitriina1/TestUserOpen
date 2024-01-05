fd2c137d1cf8bcdf4e92212223834dccbc71e6da
Dmitrii <90130328+dmitriina1@users.noreply.github.com>
2024-01-05 21:08:18 +0500

    Добавьте файлы проекта.

---
+++ Record Book MVVM/ViewModel/MainViewModel.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Input;
+using Record_Book_MVVM.Commands;
+using Record_Book_MVVM.Models;
+using Record_Book_MVVM.Views;
+
+namespace Record_Book_MVVM.ViewModel
+{
+	public class MainViewModel
+	{
+		public ObservableCollection<User> Users { get; set; }
+
+
+		public ICommand ShowWindowCommand { get; set; }
+
+
+
+		public MainViewModel()
+		{
+			Users = UserManager.GetUsers();
+
+			ShowWindowCommand = new RelayCommand(ShowWindow, CanShowWindow);
+
+		}
+
+		private bool CanShowWindow(object obj)
+		{
+			return true;
+		}
+
+		private void ShowWindow(object obj)
+		{
+			var mainWindow = obj as Window;
+
+			AddUser addUserWin = new AddUser();
+			addUserWin.Owner = mainWindow;
+			addUserWin.WindowStartupLocation = WindowStartupLocation.CenterOwner;
+			addUserWin.Show();
+
+
+		}
+        private User _selectedUser;
+        public User SelectedUser
+        {
+            get { return _selectedUser; }
+            set
+            {
+                _selectedUser = value;
+                OnPropertyChanged(nameof(SelectedUser));
+            }
+        }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        protected virtual void OnPropertyChanged(string propertyName)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+    }
+}
---
+++ Record Book MVVM/Views/Main.xaml.cs
@@ -0,0 +1,65 @@
+﻿
+using Record_Book_MVVM.Models;
+using Record_Book_MVVM.ViewModel;
+using System;
+using System.Collections.ObjectModel;
+using System.ComponentModel.Design;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+
+namespace Record_Book_MVVM.Views
+{
+	public partial class Main : Window
+	{
+		public Main()
+		{
+			InitializeComponent();
+			MainViewModel mainViewModel = new MainViewModel();
+			this.DataContext = mainViewModel;
+		}
+
+		private void FilterTextBox_TextChanged(object sender, TextChangedEventArgs e)
+		{
+
+			UserList.Items.Filter = FilterMethod;
+
+
+		}
+
+		private bool FilterMethod(object obj)
+		{
+			var user = (User)obj;
+
+			return user.Name.Contains(FilterTextBox.Text, StringComparison.OrdinalIgnoreCase);
+
+
+		}
+        private void UserListViewItem_DoubleClick(object sender, RoutedEventArgs e)
+        {
+            if (UserList.SelectedItem is User selectedUser)
+            {
+                MainViewModel mainViewModel = DataContext as MainViewModel;
+                mainViewModel.SelectedUser = selectedUser;
+
+                AddUserViewModel addUserViewModel = new AddUserViewModel();
+                addUserViewModel.SetUser(mainViewModel.SelectedUser);
+
+                AddUser addUserWin = new AddUser();
+                addUserWin.Owner = this;
+                addUserWin.DataContext = addUserViewModel;
+                addUserWin.WindowStartupLocation = WindowStartupLocation.CenterOwner;
+                addUserWin.Show();
+            }
+        }
+    }
+}
